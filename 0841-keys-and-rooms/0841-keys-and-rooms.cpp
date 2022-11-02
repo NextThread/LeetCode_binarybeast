a90bf2abea28721355f844210bed03d30ba56d52
Anurag Roy <89125578+binarybeast880@users.noreply.github.com>
2022-11-02 14:20:05 +0530

    Time: 25 ms (25.57%), Space: 10.4 MB (56.05%) - LeetHub

--- 0841-keys-and-rooms/0841-keys-and-rooms.cpp
+++ 0841-keys-and-rooms/0841-keys-and-rooms.cpp
@@ -1,5 +1,8 @@
 class Solution {
 public:
+  
+  // Idea is simple, we just need to do dfs, after applying dfs for all nodes, check if any node has not been visited, if no of visited cell == room, then true else false
+  
   void dfs(vector<vector<int>> &rooms , vector<bool> &visited , int start)    {
         visited[start] = 1;
         for(auto i : rooms[start])
@@ -7,10 +10,8 @@ public:
                 dfs(rooms , visited , i);
     }
     bool canVisitAllRooms(vector<vector<int>>& rooms) {
-        vector<bool> visited(rooms.size(), 0);        
-        int vis = 0;
+        vector<bool> visited( rooms.size() );        
         dfs(rooms , visited , 0);
-        for(auto it : visited) if(it) vis++;
-        return rooms.size() == vis;
+        return rooms.size() == count(visited.begin() , visited.end() , 1);
     }
 };
