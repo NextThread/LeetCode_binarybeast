fb7f52d98631deebca84184b97b14106f2d30681
Anurag Roy <89125578+binarybeast880@users.noreply.github.com>
2022-11-02 13:20:02 +0530

    Time: 786 ms (5.07%), Space: 92.4 MB (9.37%) - LeetHub

--- 1791-find-center-of-star-graph/1791-find-center-of-star-graph.cpp
+++ 1791-find-center-of-star-graph/1791-find-center-of-star-graph.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
+    
+    // we just need to check which node is having the highest degree
+    
     int findCenter(vector<vector<int>>& edges) {
-    map<int, int>mp;
+    map<int, int>mp; // map to store count the degree of nodes
     for(auto it : edges) 
     {
-      mp[it[0]]++;
-      mp[it[1]]++;
+      mp[it[0]]++; // increment the degree of 1st node of the pair
+      mp[it[1]]++; // increment the degree of 2nt node of the pair
     }
-      for(auto it : mp) cout<<it.first<<" occurs "<<it.second<<endl;
     int mxfreq = -1;
-    for(auto it : mp) mxfreq = max(mxfreq, it.second);
+    for(auto it : mp) mxfreq = max(mxfreq, it.second); // get the maximum degree from the map
     int res = -1;
-    for(auto it : mp) if(it.second == mxfreq) res = it.first;
-    // return edges[0][0] == edges[1][0] || edges[0][0] == edges[1][1] ? edges[0][0] : edges[0][1];
-    return res;
+    for(auto it : mp) if(it.second == mxfreq) res = it.first; // check which node is having the maximum degree
+    return res; // return it
     }
 };
