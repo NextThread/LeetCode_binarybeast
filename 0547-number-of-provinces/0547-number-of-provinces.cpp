21c4524727d406f8329b7b06fd15f4c870426f94
Anurag Roy <89125578+binarybeast880@users.noreply.github.com>
2022-11-04 14:08:51 +0530

    Time: 53 ms (52.98%), Space: 15 MB (27.38%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -3,19 +3,18 @@ public:
   
     void dfs(int src, vector<vector<int>>& isConnected, vector<int>& vis)
     {
-     vis[src] = true; 
-     vector<int>adj;
+     vis[src] = true; // make the starting node as visited
+     vector<int>adj; // to make the adjacency matrix
      for(int i = 0 ; i < isConnected.size() ; i++)
      {
-       int bit = isConnected[src][i];
-       if(bit == 1)
+       if(isConnected[src][i] == 1) // if it is one that means it has adjacent nodes, so store it in the adjacency list
        {
          adj.push_back(i);
        }
      }
       for(auto it : adj)
       {
-        if(!vis[it])
+        if(!vis[it]) // if not visited, then call dfs
         {
           dfs(it, isConnected, vis);
         }
@@ -23,14 +22,17 @@ public:
     }
   
     int findCircleNum(vector<vector<int>>& isConnected) {
+      
+      // no of times we need to call the dfs, will be our answer, because we have to count how many disjoint are their in our graph
+      
       int cnt = 0;
-      vector<int>vis(isConnected.size(), false);
+      vector<int>vis(isConnected.size(), false); // visited array, initially filled as false
       for(int i = 0 ; i < isConnected.size() ; i++)
       {
         if(!vis[i])
         {
-          cnt++;
-          dfs(i, isConnected, vis);
+          cnt++; // increment no of provinces by 1
+          dfs(i, isConnected, vis); // call dfs
         }
       }
       return cnt;
