4e0af25ae834c6c925c5d5a65f25d0e7b58daf0a
Anurag Roy <89125578+binarybeast880@users.noreply.github.com>
2022-11-07 10:44:08 +0530

    Time: 413 ms (23.80%), Space: 67.8 MB (58.73%) - LeetHub

--- 0848-shifting-letters/0848-shifting-letters.cpp
+++ 0848-shifting-letters/0848-shifting-letters.cpp
@@ -2,15 +2,14 @@ class Solution {
 public:
 
     string shiftingLetters(string s, vector<int>& shifts) {
-       int n = s.size();
        long long sum = 0;
       for(auto it : shifts)
       {
-        sum += (long long)it;
+        sum += (long long)it; // if we use accumulate(shifts.begin(), shifts.end(), 0) , it gives TLE
       }
-        for(int i = 0 ; i < n ; i++)
+        for(int i = 0 ; i < s.size() ; i++)
         {
-            s[i] = (s[i]-'a'+sum)%26+97;
+            s[i] = (s[i]-'a' + sum)%26+97;
             sum -= shifts[i];
         }
         return s;
