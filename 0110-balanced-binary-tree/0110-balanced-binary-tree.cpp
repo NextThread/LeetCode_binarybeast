d7835989fd2de423e398241192858b5e7635ca44
Anurag Roy <89125578+binarybeast880@users.noreply.github.com>
2022-11-09 20:03:12 +0530

    Time: 27 ms (49.71%), Space: 21 MB (54.64%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
@@ -1,25 +1,14 @@
-// /**
-//  * Definition for a binary tree node.
-//  * struct TreeNode {
-//  *     int val;
-//  *     TreeNode *left;
-//  *     TreeNode *right;
-//  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
-//  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-//  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
-//  * };
-//  */
-// class Solution {
-	// public:
-	// 	int height(TreeNode *root) {
-	// 		if(!root) return 0;
-	// 		return max(height(root->left), height(root->right)) + 1;
-	// 	}
-	// 	bool isBalanced(TreeNode* root) {
-	// 		if(!root) return true;
-	// 		return isBalanced(root->left) && isBalanced(root->right) && abs(height(root->left) - height(root->right)) <= 1;
-	// 	}
-// };
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
     int f(TreeNode* root)
