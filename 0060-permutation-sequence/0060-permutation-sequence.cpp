bad055d0fe0da61a45e38d065d291536bcfe79b6
Anurag Roy <89125578+binarybeast880@users.noreply.github.com>
2022-11-17 11:43:15 +0530

    Time: 4 ms (51.24%), Space: 6.2 MB (16.87%) - LeetHub

--- 0060-permutation-sequence/0060-permutation-sequence.cpp
+++ 0060-permutation-sequence/0060-permutation-sequence.cpp
@@ -1,14 +1,39 @@
 class Solution {
 public:
     string getPermutation(int n, int k) {
-      string s = "";
-      for(int i = 1 ; i <= n ; i++) s += to_string(i);
-      if(k == 1) return s; // base case: if k is 1, means the original string is the kth permutation, so return the original string
-      do{
-        next_permutation(s.begin(), s.end());
-        k--;
-      }
-      while(k > 1);
-      return s;
+      
+      /*     using STL       */
+      
+      // string s = "";
+      // for(int i = 1 ; i <= n ; i++) s += to_string(i);
+      // if(k == 1) return s; // base case: if k is 1, means the original string is the kth permutation, so return the original string
+      // do{
+      //   next_permutation(s.begin(), s.end());
+      //   k--;
+      // }
+      // while(k > 1);
+      // return s;
+      
+      
+     /*  Another method, without stl  + optimized approach  */
+    int fact = 1;
+    vector<int>nums;
+    for(int i = 1 ; i < n ; i++)
+    {
+        fact *= i;
+        nums.push_back(i);
+    }
+    nums.push_back(n);
+    k -= 1;
+    string ans = "";
+    while(1)
+    {
+        ans += to_string(nums[k/fact]);
+        nums.erase(nums.begin()+k/fact);
+        if(nums.size() == 0) break;
+        k %= fact;
+        fact /= nums.size();
+    }
+    return ans;
     }
 };
