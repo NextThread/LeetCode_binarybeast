7b959dfafd7cefb097198d78c7db059d397ff0d2
Anurag Roy <89125578+binarybeast880@users.noreply.github.com>
2022-11-17 10:55:30 +0530

    Time: 166 ms (20.92%), Space: 6 MB (52.21%) - LeetHub

--- 0060-permutation-sequence/0060-permutation-sequence.cpp
+++ 0060-permutation-sequence/0060-permutation-sequence.cpp
@@ -3,7 +3,7 @@ public:
     string getPermutation(int n, int k) {
       string s = "";
       for(int i = 1 ; i <= n ; i++) s += to_string(i);
-      if(k == 1) return s;
+      if(k == 1) return s; // base case: if k is 1, means the original string is the kth permutation, so return the original string
       do{
         next_permutation(s.begin(), s.end());
         k--;
