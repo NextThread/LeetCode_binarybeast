4c419a6c4fa09dfa2193dcb088061d380baa37d9
Anurag Roy <89125578+binarybeast880@users.noreply.github.com>
2022-11-30 14:25:43 -0500

    Time: 10 ms (61.92%), Space: 9 MB (74.49%) - LeetHub

---
+++ 443-string-compression/443-string-compression.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int compress(vector<char>& chars) {
+    int cnt = 1;
+    string res;
+    for(int i=0;i<chars.size();i++)
+    {
+        while(i<chars.size()-1 && chars[i+1] == chars[i])
+        {
+            cnt++;
+            i++;
+        }
+        res += chars[i];
+        if(cnt == 1)
+        {
+            continue;
+        }
+        res += to_string(cnt);
+        cnt = 1;
+     }
+     chars.clear();
+     for(int i = 0 ; i < res.size() ; i++)
+     {
+          chars.push_back(res[i]);
+     }
+     return chars.size();
+    }
+};
