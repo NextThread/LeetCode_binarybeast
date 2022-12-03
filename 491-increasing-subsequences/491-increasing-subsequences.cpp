f993eb0bd0cd2f31575064dd1cfcb5e1aedb4c2e
Anurag Roy <89125578+binarybeast880@users.noreply.github.com>
2022-12-03 06:13:02 -0500

    Time: 33 ms (99.26%), Space: 16.4 MB (96.30%) - LeetHub

---
+++ 491-increasing-subsequences/491-increasing-subsequences.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    void solving(vector<int>& nums, vector<vector<int>>& output, vector<int>& temp, int index){
+        if(index >= nums.size()){ // base case
+            if(temp.size() > 1)
+                output.push_back(temp);
+            return;
+        }
+        if(index == 0 || temp.size() == 0 || nums[index] >= temp[temp.size()-1]){ // if we met the condition
+            temp.push_back(nums[index]); // store the subset in our res
+            solving(nums, output, temp, index+1); // recursive call for next index
+            temp.pop_back(); // pop the last one, so that we can backtrack from the last location
+        }
+        if(temp.size() ==0 || temp[temp.size()-1] != nums[index]) {
+            solving(nums, output, temp, index+1);
+        }
+    }
+	
+    vector<vector<int>> findSubsequences(vector<int>& nums) {
+        vector<vector<int>> output; // where we are going to store the subsets to return it
+        vector<int> temp;
+        solving(nums, output, temp, 0); // dfs call
+        return output;
+    }
+};
