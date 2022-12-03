8149615c9d4b6d9d488fa8edc7205cbe405c5366
Anurag Roy <89125578+binarybeast880@users.noreply.github.com>
2022-12-02 22:15:19 -0500

    Time: 28 ms (48.52%), Space: 8.6 MB (40.07%) - LeetHub

--- 451-sort-characters-by-frequency/451-sort-characters-by-frequency.cpp
+++ 451-sort-characters-by-frequency/451-sort-characters-by-frequency.cpp
@@ -1,25 +1,20 @@
 class Solution {
 public:
     
-    static bool cmp(pair<char,int>&a, pair<char,int>&b) {
+    static bool cmp(pair<char,int>&a, pair<char,int>&b) { // comparator, which we are going to use to sort our map
         return a.second>b.second;
     }
     
     string frequencySort(string s) {
-        if(s.length()<2) 
-            return s;
-        map<char,int> mp;
-        for(int i=0;i<s.length();i++) {
-            mp[s[i]]++;
-        }
-        vector<pair<char,int>> freq_pair;
-        for(auto m: mp) {
-            freq_pair.push_back(m);
-        }
-        sort(freq_pair.begin(),freq_pair.end(),cmp);
-        string ans;
-        for(auto f : freq_pair) {
-            for(int i=0;i<f.second;i++) {
+        if(s.length() < 2) return s; // base case
+        unordered_map<char, int> mp; // map to store the frequency of each character
+        for(char ch : s) mp[ch]++; // count the frequency of each character
+        vector<pair<char,int>> freq_pair; // we are going to store the {char, freq} pair in a vector, so that we can sort it using our comparator.
+        for(auto pairs : mp) freq_pair.push_back(pairs); // store in the vector
+        sort(freq_pair.begin(), freq_pair.end(), cmp); // sort the vector
+        string ans = "";
+        for(auto f : freq_pair) { // iterate on the vector, and keep adding chars into our ans string, and then return the string
+            for(int i = 0 ; i < f.second ; i++) {
                 ans += f.first;
             }
         }
