7675b9fdbb421ae6899ff41e49b79f7eb7d55b21
Anurag Roy <89125578+binarybeast880@users.noreply.github.com>
2022-12-04 23:58:34 -0500

    Time: 4 ms (49.93%), Space: 7 MB (72.02%) - LeetHub

--- 876-middle-of-the-linked-list/876-middle-of-the-linked-list.cpp
+++ 876-middle-of-the-linked-list/876-middle-of-the-linked-list.cpp
@@ -11,6 +11,13 @@
 class Solution {
 public:
     ListNode* middleNode(ListNode* head) {
+        
+        // This is a perfect example of two pointer approach
+        // we will be keeping two pointer as slow and fast
+        // slow will get incremented by one step 
+        // fast will get incremented by two at each step
+        // at last when our fast stands at last, then our slow will be pointing to the mid
+        // so we just return the slow pointer, which is currently pointing to the middle node
         if(!head) return NULL;
         if(!head->next) return head;
         ListNode* slow = head, *fast = head;
