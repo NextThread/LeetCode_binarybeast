3eba0f88bcfff415f2df96e84c0558ce3c9a13aa
Anurag Roy <89125578+binarybeast880@users.noreply.github.com>
2022-12-24 23:43:41 -0500

    Time: 18 ms (93.30%), Space: 13.7 MB (83.30%) - LeetHub

--- 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.cpp
+++ 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.cpp
@@ -3,10 +3,9 @@ public:
     vector<int> answerQueries(vector<int>& A, vector<int>& queries) {
       sort(A.begin(), A.end());
         vector<int> res;
-        for (int i = 1; i < A.size(); ++i)
-            A[i] += A[i - 1];
-        for (int& q: queries)
-            res.push_back(upper_bound(A.begin(), A.end(), q) - A.begin());
+        for (int i = 1; i < A.size(); ++i) A[i] += A[i - 1]; // update A into prefix array
+        for (int q: queries)
+            res.push_back(upper_bound(A.begin(), A.end(), q) - A.begin()); // binary search
         return res;
     }
 };
