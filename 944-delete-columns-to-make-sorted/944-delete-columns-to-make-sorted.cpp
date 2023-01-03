544ce60bcfe8559692c055057005283ac249d4e5
Anurag Roy <89125578+NextThread@users.noreply.github.com>
2023-01-03 10:17:41 +0530

    Time: 52 ms (69.15%), Space: 12.2 MB (72.46%) - LeetHub

--- 944-delete-columns-to-make-sorted/944-delete-columns-to-make-sorted.cpp
+++ 944-delete-columns-to-make-sorted/944-delete-columns-to-make-sorted.cpp
@@ -5,7 +5,7 @@ public:
         for (int i = 0 ; i < strs[0].size(); i++)
         for (int j = 1; j < strs.size(); j++) {
             if (strs[j - 1][i] > strs[j][i]) {
-                ++cnt;
+                cnt+=1;
                 break;
             }
         }
