1d8aebea9c6fcfcdf897c681825f18728ca1a8fc
Anurag Roy <89125578+NextThread@users.noreply.github.com>
2023-01-08 09:41:36 +0530

    Time: 33 ms (90.17%), Space: 16.9 MB (39.11%) - LeetHub

--- 149-max-points-on-a-line/149-max-points-on-a-line.cpp
+++ 149-max-points-on-a-line/149-max-points-on-a-line.cpp
@@ -6,16 +6,16 @@ public:
             unordered_map <long double, int> h;
             int same = 1, localmax = 0;
             for(int j = i+1 ; j < points.size() ; ++j) {
-                if(points[i][0] == points[j][0] && points[i][1] == points[j][1]) same++;
-                else if(points[i][0] == points[j][0]) h[INT_MAX]++;
+                if(points[i][0] == points[j][0] && points[i][1] == points[j][1]) same++; // both are on same point
+                else if(points[i][0] == points[j][0]) h[INT_MAX]++; // both are on same line
                 else{
                     long double slope = (long double)(points[i][1] - points[j][1]) / (long double)(points[i][0] - points[j][0]);
-                    h[slope]++;
+                    h[slope]++; // increase the counter for the slope
                 }
             }
-            for(auto i : h) localmax = max(localmax,i.second);
-            localmax +=same;
-            result = max(result, localmax);
+            for(auto i : h) localmax = max(localmax, i.second); // pickup the max slope count
+            localmax +=same; // add those points that are at same point
+            result = max(result, localmax); // update the result
         }
         return result;
     }
