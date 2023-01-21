7f092db98b7d2bbe05db17fd879363996ca9b64b
Anurag Roy <89125578+NextThread@users.noreply.github.com>
2023-01-21 09:52:34 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (89.32%) - LeetHub

--- 93-restore-ip-addresses/93-restore-ip-addresses.cpp
+++ 93-restore-ip-addresses/93-restore-ip-addresses.cpp
@@ -12,16 +12,16 @@ public:
                 {
                     for(int l = 1 ; l <= 3 ; l++)
                     {
-                        if(i+j+k+l == s.length())
+                        if(i+j+k+l == s.length()) // if we have 4 substring that is equal string length
                         {
-                            int part1 = stoi(s.substr(0, i));
+                            int part1 = stoi(s.substr(0, i)); // convert the 4 substring into integers to check if they are in range
                             int part2 = stoi(s.substr(i, j));
                             int part3 = stoi(s.substr(i+j, k));
                             int part4 = stoi(s.substr(i+j+k, l));
-                            if(part1 <= 255 and part2 <= 255 and part3 <= 255 and part4 <= 255)
+                            if(part1 <= 255 and part2 <= 255 and part3 <= 255 and part4 <= 255) // if all the parts are in range 
                             {
-                                string ip = to_string(part1)+"."+to_string(part2)+"."+to_string(part3)+"."+to_string(part4);
-                                if(ip.length() == s.length()+3)
+                                string ip = to_string(part1)+"."+to_string(part2)+"."+to_string(part3)+"."+to_string(part4); // convert this into an IP
+                                if(ip.length() == s.length()+3) // +3 is for extra "." dots that we have inserted inside the IP
                                 {
                                     res.push_back(ip);
                                 }
