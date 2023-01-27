c87e1efa891b088564f47e414bfbc8692736f43f
Anurag Roy <89125578+NextThread@users.noreply.github.com>
2023-01-27 09:58:03 +0530

    Time: 229 ms (77.28%), Space: 55.1 MB (48.05%) - LeetHub

--- 472-concatenated-words/472-concatenated-words.cpp
+++ 472-concatenated-words/472-concatenated-words.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
-     bool concatenatePossible(string word, unordered_set<string>& dict, unordered_map<string, bool> &memo){
-        if(memo.find(word)!= memo.end()){
-            return memo[word];
-        }
-        for(int i=1; i<word.size(); ++i){
-            string substring = word.substr(0, i);
-            if(dict.count(substring)){
-                string restSubstring = word.substr(i);
-                if(dict.count(restSubstring) || concatenatePossible(restSubstring, dict, memo)){
+     bool check(string word, unordered_set<string>& dict, unordered_map<string, bool> &memo){
+        if(memo.find(word) !=  memo.end()) return memo[word];
+        for(int i = 1 ; i < word.size() ; ++i) {
+            string substring = word.substr(0, i); // check for every substring
+            if(dict.count(substring)) { // if substring is pre-stored
+                string nextpart = word.substr(i); // the rest substring
+                if(dict.count(nextpart) || check(nextpart, dict, memo)) // check if the next substring is been there in our map already or using the next part we can do the same operation(recursive call) and is possible
+                {
                     memo.insert({word, true});
                     return true;
                 }
@@ -19,14 +18,11 @@ public:
     }
     
     vector<string> findAllConcatenatedWordsInADict(vector<string>& words) {
-        vector<string> ans;
-        unordered_set<string> dict(words.begin(), words.end());
-        unordered_map<string, bool> memo;
-        for(auto word : words){
-            if(concatenatePossible(word, dict, memo)){
-                ans.push_back(word);
-            }
-        }
-        return ans;
+        vector<string> res;
+        unordered_set<string> dict(words.begin(), words.end()); // let's remove duplicate words first
+        unordered_map<string, bool> mp;
+        for(string word : words)
+            if(check(word, dict, mp)) res.push_back(word);
+        return res;
     }
 };
