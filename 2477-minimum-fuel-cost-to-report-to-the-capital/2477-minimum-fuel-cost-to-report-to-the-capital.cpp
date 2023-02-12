ce8d8f91d455bb602777bbfd4157a86a6c4541f3
Anurag Roy <89125578+NextThread@users.noreply.github.com>
2023-02-12 08:55:42 +0530

    Time: 600 ms (82.67%), Space: 191.5 MB (72.79%) - LeetHub

--- 2477-minimum-fuel-cost-to-report-to-the-capital/2477-minimum-fuel-cost-to-report-to-the-capital.cpp
+++ 2477-minimum-fuel-cost-to-report-to-the-capital/2477-minimum-fuel-cost-to-report-to-the-capital.cpp
@@ -1,9 +1,12 @@
 class Solution {
 public:
-    long long ans = 0; int s;
+    long long ans = 0; 
+    int s;
 long long minimumFuelCost(vector<vector<int>>& roads, int seats) {
-    vector<vector<int>> graph(roads.size() + 1); s = seats;
-    for (vector<int>& r: roads) {
+    vector<vector<int>> graph(roads.size() + 1);
+    s = seats;
+    for (vector<int>& r: roads) 
+    {
         graph[r[0]].push_back(r[1]);
         graph[r[1]].push_back(r[0]);
     }
@@ -11,11 +14,14 @@ long long minimumFuelCost(vector<vector<int>>& roads, int seats) {
     return ans;
 }
 int dfs(int i, int prev, vector<vector<int>>& graph, int people = 1) {
-    for (int& x: graph[i]) {
-        if (x == prev) continue;
+    for (int& x: graph[i])
+    {
+        if (x == prev)
+            continue;
         people += dfs(x, i, graph);
     }
-    if (i != 0) ans += (people + s - 1) / s;
+    if (i != 0) 
+        ans += (people + s - 1) / s;
     return people;
 }
 };
