352693ac91ae28c930adcab46ff336f3d9b95031
Anurag Roy <89125578+NextThread@users.noreply.github.com>
2023-02-24 08:54:05 -0500

    Time: 357 ms (82.20%), Space: 89.1 MB (37.29%) - LeetHub

---
+++ 1675-minimize-deviation-in-array/1675-minimize-deviation-in-array.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int minimumDeviation(vector<int>& A) {
+        set<int> s;
+        for (int &a : A)
+            s.insert(a % 2 ? a * 2 : a);
+        int res = *s.rbegin() - *s.begin();
+        while (*s.rbegin() % 2 == 0) {
+            s.insert(*s.rbegin() / 2);
+            s.erase(*s.rbegin());
+            res = min(res, *s.rbegin() - *s.begin());
+        }
+        return res; 
+    }
+};
