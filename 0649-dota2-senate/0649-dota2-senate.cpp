6c05ad5edd59134024b30f4fb02bd78cefb92208
Anurag Roy <89125578+NextThread@users.noreply.github.com>
2023-05-04 17:49:49 +0530

    Time: 0 ms (100.00%), Space: 7.3 MB (71.76%) - LeetHub

--- 0649-dota2-senate/0649-dota2-senate.cpp
+++ 0649-dota2-senate/0649-dota2-senate.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     string predictPartyVictory(string senate) {
         int count = 0, len = 0;
-        // When the length of senate doesn't decrease, the game is over.
         while (senate.size() != len) {
             string s;
             len = senate.size();
