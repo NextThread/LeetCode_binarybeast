27cd1514bc231554028f40dd9da4429ee90b9684
Anurag Roy <89125578+NextThread@users.noreply.github.com>
2023-06-07 18:51:42 +0530

    Added solution - LeetHub

---
+++ Kth smallest element - GFG/kth-smallest-element.cpp
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+//Initial function template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution{
+    public:
+    // arr : given array
+    // l : starting index of the array i.e 0
+    // r : ending index of the array i.e size-1
+    // k : find kth smallest element and return using this function
+    int kthSmallest(int arr[], int l, int r, int k) {
+        priority_queue<int> pq;
+        // priority_queue<int, vector<int>, greater<int> > pq;
+        
+        for(int i = l ; i <= r ; i++) {
+            pq.push(arr[i]);
+        }
+        
+        while(pq.size() > k) {
+            pq.pop();
+        }
+        
+        return pq.top();
+    }
+};
+
+//{ Driver Code Starts.
+ 
+int main()
+{
+    int test_case;
+    cin>>test_case;
+    while(test_case--)
+    {
+        int number_of_elements;
+        cin>>number_of_elements;
+        int a[number_of_elements];
+        
+        for(int i=0;i<number_of_elements;i++)
+            cin>>a[i];
+            
+        int k;
+        cin>>k;
+        Solution ob;
+        cout<<ob.kthSmallest(a, 0, number_of_elements-1, k)<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
