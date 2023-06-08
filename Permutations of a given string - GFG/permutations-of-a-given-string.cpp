402952ad097603e9e9e1f8c3735ce53a2b419a06
Anurag Roy <89125578+NextThread@users.noreply.github.com>
2023-06-09 00:33:36 +0530

    Added solution - LeetHub

---
+++ Permutations of a given string - GFG/permutations-of-a-given-string.cpp
@@ -0,0 +1,41 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+	public:
+		vector<string>find_permutation(string S)
+		{
+		    sort(S.begin(), S.end());
+		    vector<string> res;
+		    do {
+		        res.push_back(S);
+		    } while(next_permutation(S.begin(), S.end()));
+		    return res;
+		}
+};
+
+
+
+//{ Driver Code Starts.
+int main(){
+    int t;
+    cin >> t;
+    while(t--)
+    {
+	    string S;
+	    cin >> S;
+	    Solution ob;
+	    vector<string> ans = ob.find_permutation(S);
+	    for(auto i: ans)
+	    {
+	    	cout<<i<<" ";
+	    }
+	    cout<<"\n";
+    }
+	return 0;
+}
+
+// } Driver Code Ends
