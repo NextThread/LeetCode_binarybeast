3ebfdebb601e4f867c145e8ed405e31a48d6c540
Anurag Roy <89125578+NextThread@users.noreply.github.com>
2023-06-18 18:09:44 +0530

    Added solution - LeetHub

---
+++ Third largest element - GFG/third-largest-element.cpp
@@ -0,0 +1,37 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+  public:
+    int thirdLargest(int a[], int n)
+    {
+         sort(a, a+n);
+        //   1 2 3 4 5
+        // 5 4 3 2 1
+         reverse(a, a+n);
+         
+         return a[2];
+    }
+
+};
+
+//{ Driver Code Starts.
+ 
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+	    int n;
+	    cin>>n;
+	    int a[n];
+	    for(int i=0;i<n;i++)
+	        cin>>a[i];
+	   Solution obj;
+	    cout<<obj.thirdLargest(a,n)<<endl;
+    }
+}   
+// } Driver Code Ends
