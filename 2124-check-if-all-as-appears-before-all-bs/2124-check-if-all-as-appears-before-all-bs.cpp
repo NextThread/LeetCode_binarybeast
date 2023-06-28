b8f53e4fbd24e732c92a1d2f3bd0ed13dab34390
Anurag Roy <89125578+NextThread@users.noreply.github.com>
2023-06-28 13:23:41 +0530

    Time: 0 ms (100.00%), Space: 6 MB (100.00%) - LeetHub

--- 2124-check-if-all-as-appears-before-all-bs/2124-check-if-all-as-appears-before-all-bs.cpp
+++ 2124-check-if-all-as-appears-before-all-bs/2124-check-if-all-as-appears-before-all-bs.cpp
@@ -1,11 +1,6 @@
 class Solution {
 public:
     bool checkString(string s) {
-        for(int i = 0 ; i < s.size()-1 ; i++) {
-            if(s[i] == 'b' and s[i+1] == 'a') {
-                return false;
-            }
-        }
-        return true;
+        return (s.find("ba") == string::npos);
     }
 };
