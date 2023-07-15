524f32fd68adb444ce5495fad4b1751db33185cc
Anurag Roy <89125578+NextThread@users.noreply.github.com>
2023-07-15 10:23:31 +0530

    Time: 294 ms (94.00%), Space: 75.8 MB (60.35%) - LeetHub

--- 1751-maximum-number-of-events-that-can-be-attended-ii/1751-maximum-number-of-events-that-can-be-attended-ii.cpp
+++ 1751-maximum-number-of-events-that-can-be-attended-ii/1751-maximum-number-of-events-that-can-be-attended-ii.cpp
@@ -1,31 +1,41 @@
 class Solution {
+
 public:
-    int solve(vector<vector<int>>&dp, vector<vector<int>>& events, int n, int pos, int k)
-{
+    
+    int recur(vector<vector<int>>&dp,int idx,vector<vector<int>>&events,int k)
+    {
+        if(idx>=events.size() or k==0)
+            return 0;
         int i;
         
-        if(pos >= n || k == 0)
-            return 0;
-        
-        if(dp[pos][k] != -1)
-            return dp[pos][k];
-        
-        // finding the next event which we can attend
-        for(i = pos + 1; i < n; i++)
-            if(events[i][0] > events[pos][1])
+        if(dp[idx][k]!=-1)
+            return dp[idx][k];
+        for(i=idx+1;i<events.size();i++)
+        {
+            if(events[i][0]>events[idx][1])
+            {
                 break;
+            }
+        }
+        int op1=recur(dp,idx+1,events,k);
+        int op2=events[idx][2]+recur(dp,i,events,k-1);
         
-       return dp[pos][k] = max(solve(dp, events, n, pos+1, k), events[pos][2] + solve(dp, events, n, i, k-1));
-
+        return dp[idx][k]=max(op1,op2);
     }
     
-    int maxValue(vector<vector<int>>& events, int k) 
-    {
-        int n = events.size();
-        sort(events.begin(), events.end());
-        
+    int maxValue(vector<vector<int>>& events, int k) {
+        sort(events.begin(),events.end(),[]
+            (vector<int>&a,vector<int>&b)
+             {
+                 if(a[0]==b[0])
+                     return a[1]<b[1];
+                 return  a[0]<b[0];
+             }
+            );
+        int n=events.size();
         vector<vector<int>>dp(n+1,vector<int>(k+1,-1));
+        return recur(dp,0,events,k);
         
-        return solve(dp, events, n, 0, k);
+        
     }
 };
