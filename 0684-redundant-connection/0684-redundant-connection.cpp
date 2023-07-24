e1d0c742173c4e9ddd15ab9da5f1401746afa8f5
Anurag Roy <89125578+NextThread@users.noreply.github.com>
2023-07-24 11:07:43 +0530

    Time: 8 ms (73.38%), Space: 8.8 MB (78.68%) - LeetHub

---
+++ 0684-redundant-connection/0684-redundant-connection.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> par;
+    int find(int x) {
+        if (x != par[x]) par[x] = find(par[x]);
+        return par[x];
+    }
+    void uniun(int x, int y) {
+        par[find(y)] = find(x);
+    }
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+        par.resize(edges.size()+1);
+        for (int i = 0; i < par.size(); i++)
+            par[i] = i;
+        for (auto& e : edges)
+            if (find(e[0]) == find(e[1])) return e;
+            else uniun(e[0],e[1]);
+        return {};
+    }  
+};
