2896ef1952179238b7ad65220c8a44290584d0e8
Anurag Roy <89125578+NextThread@users.noreply.github.com>
2023-08-21 11:42:35 +0530

    Time: 16 ms (79.18%), Space: 12.7 MB (76.82%) - LeetHub

--- 0459-repeated-substring-pattern/0459-repeated-substring-pattern.cpp
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.cpp
@@ -1,35 +1,13 @@
 class Solution {
 public:
-    bool repeatedSubstringPattern(string s) {
-        
-        int n=s.length();
-        vector<int> dp(n,0);
-        
-        int i=1;
-        int j=0;
-        
-        for(i=1;i<n;)
-        {
-            if(s[i]==s[j])
-            {
-                j++;
-                dp[i]=j;
-                i++;
-            }
-            else
-            {
-                if(j!=0)
-                {
-                    j=dp[j-1];
-                }
-                else
-                {
-                    dp[i]=0;
-                    i++;
-                }
-            }
+     bool repeatedSubstringPattern(string str) {
+        int i = 1, j = 0, n = str.size();
+        vector<int> dp(n+1,0);
+        while( i < str.size() ){
+            if( str[i] == str[j] ) dp[++i]=++j;
+            else if( j == 0 ) i++;
+            else j = dp[j];
         }
-        
-        return dp[n-1] && (dp[n-1]%(n-dp[n-1])==0);
+        return dp[n]&&dp[n]%(n-dp[n])==0;
     }
 };
