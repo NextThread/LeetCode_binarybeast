613d07a97aaf18975b6c75bb9569b4514321b778
Anurag Roy <89125578+NextThread@users.noreply.github.com>
2023-09-15 15:21:34 +0530

    Time: 348 ms (68.71%), Space: 58.3 MB (62.52%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
@@ -1,22 +1,66 @@
 class Solution {
 public:
-   int minCostConnectPoints(vector<vector<int>>& ps) {
-    int n = ps.size(), res = 0, i = 0, connected = 0;
-    vector<int> min_d(n, 10000000);
-    while (++connected < n) { // jab tak sbhi points connect nhi ho jaate tab tak kaam karo
-        min_d[i] = INT_MAX; // kyuki min distance set krna hai, to max se initialize karo
-        int curr = i; // current node'
-        for (int j = 0; j < n; j++) // uske every neighbourse k distance calculate karo, and minimum ko lo
-            if (min_d[j] != INT_MAX) 
-            {
-                min_d[j] = min(min_d[j], abs(ps[i][0] - ps[j][0]) + abs(ps[i][1] - ps[j][1]));
-                if(min_d[j] < min_d[curr]) {
-                    curr = j;
-                }
-            }
-        res += min_d[curr]; // res k saath wahi minimum distance add karo
-        i = curr; // i ko abhi k current node pe set krdo
+    int par[1001]; // parent array to store parent of every node
+    
+    // Kruskal uses DSU
+    
+    int find(int a) // find function to find parent of a node
+    {
+        if(par[a] < 0)
+            return a;
+        
+        return par[a] = find(par[a]); // path compression
     }
-    return res;
-}
+    
+    // Union function to make one node parent of anthor
+    void Union(int a, int b)
+    {
+        par[a] = b; // here we are making parent of a to b
+    }
+    
+    int minCostConnectPoints(vector<vector<int>>& arr) {
+        int n = arr.size(); // extract the size of array
+        
+        // Intially all are alone, so everyone is -1
+        for(int i = 0; i < n; i++) par[i] = -1;
+        
+        // define adj vector to store three things
+        // first value store weight where
+        // weight is actually manhattan distance
+        
+        vector<pair<int, pair<int, int>>> adj;
+        
+        // generating graph basically with weights
+        for(int i = 0; i < n; i++)
+        {
+            for(int j = i + 1; j < n; j++)
+            {
+                int weight = abs(arr[i][0] - arr[j][0]) + 
+                             abs(arr[i][1] - arr[j][1]);//manhattan distance
+                
+                adj.push_back({weight, {i, j}});
+                
+            }
+        }
+        
+        // sort on the basis of their edge weight
+        sort(adj.begin(), adj.end());
+        
+        int sum = 0; //intially define sum as zero
+        
+        // for each edge we travel
+        for(int i = 0; i < adj.size(); i++)
+        {
+            int a = find(adj[i].second.first); // first node
+            int b = find(adj[i].second.second); // second node
+            
+            if(a != b) // if both parents are not same then add weight
+            {
+                sum += adj[i].first;
+                Union(a, b); // now merge them
+            }
+        }
+        
+        return sum; // finally return sum
+    }
 };
