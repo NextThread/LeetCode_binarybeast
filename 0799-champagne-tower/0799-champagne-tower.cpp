156fc811a0415d2a5683dd283d77ce636f7bce8e
Anurag Roy <89125578+NextThread@users.noreply.github.com>
2023-09-24 09:04:32 +0530

    Time: 4 ms (80.02%), Space: 6.5 MB (73.98%) - LeetHub

--- 0799-champagne-tower/0799-champagne-tower.cpp
+++ 0799-champagne-tower/0799-champagne-tower.cpp
@@ -1,14 +1,56 @@
 class Solution {
 public:
+    double t[101][101]; // declaring 2 d array, to store some already computed results
+    double solve(int poured, int row, int glass)
+    {
+        // if any of the condition fails
+        if(row < 0 || glass > row || glass < 0)
+        {
+            return 0.00;
+        }
+        
+        // if we reach at our target 
+        if(row == 0 && glass == 0)
+        {
+            return poured;
+        }
+        
+        // if result is already computed, then return from here
+        // this one line which we say to add
+        if(t[row][glass] > -1)
+        {
+            return t[row][glass];
+        }
+        
+        // how much comes from left part- first coordinate(i -1, j - 1) then keep 1 for themself and then divide by 2
+        double left = (solve(poured, row - 1, glass - 1) - 1) / 2;
+        
+        // if comes negative then, in reality zero comes
+        if(left < 0)
+        {
+            left = 0;
+        }
+        
+        // how much comes from right part- first coordinate(i - 1, j) then keep 1 for themself and then divide by 2
+        double right = (solve(poured, row - 1, glass) - 1) / 2;
+        
+        // if comes negative then, in reality zero comes
+        if(right < 0)
+        {
+            right = 0;
+        }
+        
+        // and add coming from left and right
+        double total = left + right;
+        
+        return t[row][glass] = total;// and finally return total
+        
+        
+    }
     double champagneTower(int poured, int query_row, int query_glass) {
-        vector<double> res(101);
-        res[0] = poured;
-        for (int row = 1; row <= query_row; row++)
-            for (int i = row; i >= 0; i--) {
-                res[i] = max(0.0, (res[i] - 1) / 2);
-                res[i + 1] += res[i];
-            }
-                
-        return min(res[query_glass], 1.0);
+        memset(t, -1, sizeof(t));
+        
+        // solve funcion which return answer
+        return min(1.00, solve(poured, query_row, query_glass));
     }
 };
