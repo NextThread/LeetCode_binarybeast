2a39ab7f16afa6a2c00799c6c0b167bafbf8ba54
Anurag Roy <89125578+NextThread@users.noreply.github.com>
2023-09-29 10:58:51 +0530

    Time: 2 ms (45.03%), Space: 6.2 MB (65.89%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
+    
     int dp[101][101];
+    
     int rec(int i, int j, int m, int n) {
-        if(i < 0 or j < 0 or i > m or j > n) return 0;
+        if(i > m or j > n) return 0;
         if(i == m and j == n) return 1;
         if(dp[i][j] != -1) return dp[i][j];
-        int right = rec(i, j+1, m, n);
-        int down = rec(i+1, j, m, n);
-        return dp[i][j] = right + down;
+        int op1 = rec(i+1, j, m, n);
+        int op2 = rec(i, j+1, m, n);
+        return dp[i][j] = op1+op2;
     }
     
     int uniquePaths(int m, int n) {
