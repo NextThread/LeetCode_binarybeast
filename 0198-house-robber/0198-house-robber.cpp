32e2324fd1a0eb4fe3a31bf8964d0e8b1c19498d
Anurag Roy <89125578+NextThread@users.noreply.github.com>
2023-09-29 10:46:14 +0530

    Time: 0 ms (100.00%), Space: 8.1 MB (10.55%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,24 +1,18 @@
 class Solution {
 public:
-    int dp[100];
-    int f(int i, vector<int> &nums) {
+    
+    int dp[101];
+    
+    int rec(int i, vector<int> &nums) {
         if(i >= nums.size()) return 0;
         if(dp[i] != -1) return dp[i];
-        int pick = nums[i] + f(i+2, nums);
-        int notpick = 0 + f(i+1, nums);
-        return dp[i] = max(pick, notpick);
+        int chori = nums[i] + rec(i+2, nums);
+        int skip = 0 + rec(i+1, nums);
+        return dp[i] = max(chori, skip);
     }
     
     int rob(vector<int>& nums) {
-        // memset(dp, -1, sizeof(dp));
-       // return f(0, nums); 
-        dp[0] = nums[0];
-        if(nums.size() == 1) return dp[0];
-        dp[1] = max(nums[0], nums[1]);
-        if(nums.size() == 2) return dp[1];
-        for(int i = 2 ; i < nums.size() ; i++) {
-            dp[i] = max(dp[i-1], dp[i-2] + nums[i]);
-        }
-        return dp[nums.size()-1];
+        memset(dp, -1, sizeof dp);
+        return rec(0, nums);
     }
 };
