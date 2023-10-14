71b424137e4b0cd8b75ff974324dbbbf7b3eb43e
Anurag Roy <89125578+NextThread@users.noreply.github.com>
2023-10-14 10:15:20 +0530

    Time: 165 ms (49.77%), Space: 90 MB (80.81%) - LeetHub

--- 2742-painting-the-walls/2742-painting-the-walls.cpp
+++ 2742-painting-the-walls/2742-painting-the-walls.cpp
@@ -1,20 +1,18 @@
 class Solution {
+    int dp[501][501];
 private:
-    int memo(vector<int>&cost, vector<int> &time, int index, int walls, vector<vector<int>> &dp) {
+    int memo(vector<int>&cost, vector<int> &time, int index, int walls) {
         if(walls <= 0) return 0;//can be one of the possible way
-        if(index < 0 ) return 1e9;//Neglect this by returning unusually large value
+        if(index >= cost.size()) return 1e9;//Neglect this by returning unusually large value
         if(dp[index][walls] != -1) return dp[index][walls];
-
-        int notTake = 0 + memo(cost, time, index - 1, walls, dp);
-        int take = cost[index] + memo(cost, time, index - 1, walls- time[index] - 1, dp);
+        int notTake = 0 + memo(cost, time, index + 1, walls);
+        int take = cost[index] + memo(cost, time, index + 1, walls - time[index] - 1);
         return dp[index][walls] = min(notTake, take);
     }
 public:
     int paintWalls(vector<int>& cost, vector<int>& time) {
         int n = cost.size();
-        vector<vector<int>> dp(n + 1, vector<int>(n + 1, -1));
-        return memo(cost, time, n-1, n, dp);
-
-        
+        memset(dp, -1, sizeof dp);
+        return memo(cost, time, 0, n);
     }
 };
