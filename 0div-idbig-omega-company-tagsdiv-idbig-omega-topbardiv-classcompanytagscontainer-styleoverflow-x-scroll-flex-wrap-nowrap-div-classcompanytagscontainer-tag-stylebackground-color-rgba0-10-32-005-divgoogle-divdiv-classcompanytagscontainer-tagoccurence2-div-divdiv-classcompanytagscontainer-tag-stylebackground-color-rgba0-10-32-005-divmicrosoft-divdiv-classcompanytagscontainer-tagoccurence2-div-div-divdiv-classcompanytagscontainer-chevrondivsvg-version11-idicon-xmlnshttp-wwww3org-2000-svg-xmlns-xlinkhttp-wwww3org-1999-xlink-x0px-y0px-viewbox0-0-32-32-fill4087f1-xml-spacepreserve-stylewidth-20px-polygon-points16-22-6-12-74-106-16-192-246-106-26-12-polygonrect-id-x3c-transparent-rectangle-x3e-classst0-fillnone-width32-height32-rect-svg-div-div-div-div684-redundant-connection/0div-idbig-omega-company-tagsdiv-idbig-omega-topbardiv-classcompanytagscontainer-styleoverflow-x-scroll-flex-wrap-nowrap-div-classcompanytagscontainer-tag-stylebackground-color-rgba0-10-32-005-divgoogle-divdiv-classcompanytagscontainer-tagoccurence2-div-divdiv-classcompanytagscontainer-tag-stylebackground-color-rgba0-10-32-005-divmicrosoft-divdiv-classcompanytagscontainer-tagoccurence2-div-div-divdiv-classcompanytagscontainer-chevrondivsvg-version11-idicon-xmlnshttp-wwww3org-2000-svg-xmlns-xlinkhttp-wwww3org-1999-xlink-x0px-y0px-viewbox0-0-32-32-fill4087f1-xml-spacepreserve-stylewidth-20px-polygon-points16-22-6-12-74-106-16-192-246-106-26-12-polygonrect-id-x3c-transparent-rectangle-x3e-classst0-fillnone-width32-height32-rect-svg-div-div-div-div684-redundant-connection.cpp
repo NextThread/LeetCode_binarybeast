7b73f336c9b8eae05790922ed4a7019ecfad5575
Anurag Roy <89125578+NextThread@users.noreply.github.com>
2023-12-24 18:19:05 +0530

    Time: 33 ms (5.23%), Space: 10.1 MB (18.63%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence2-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divmicrosoft-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div684-redundant-connection/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence2-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divmicrosoft-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div684-redundant-connection.cpp
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence2-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divmicrosoft-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div684-redundant-connection/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence2-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divmicrosoft-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div684-redundant-connection.cpp
@@ -1,30 +1,29 @@
-class DSU {
-    vector<int> par, rank;
-public:
-    DSU(int n) : par(n), rank(n) {
-        iota(begin(par), end(par), 0);      // initializes each node's parent to be itself - fills as [0,1,2,3,...,n]
-    }
-    int find(int x) {
-        if(x == par[x]) return x;           // x is itself the parent of this component
-        return find(par[x]);                // recurse for parent of x
-    }
-    bool Union(int x, int y) {
-        auto xp = find(x), yp = find(y);    // find parents of x and y, i.e, representatives of components that x and y belong to
-        if(xp == yp) return false;          // x and y already belong to same component - not possible to union
-        return par[xp] = yp;                // union x and y by making parent common
-    }
-    // iterative implementation of find
-    // int find(int x) {  
-    //     while(x != par[x]) x = par[x];
-    //     return par[x];
-    // }
-};
 class Solution {
 public:
-    vector<int> findRedundantConnection(vector<vector<int>>& e) {
-        DSU ds(size(e)+1);
-        for(auto& E : e) 
-            if(!ds.Union(E[0], E[1])) return E;	// not possible to union - adding this edge was causing the cycle
-        return { };    // un-reachable
+    
+    bool dfs(int node, int par, vector<vector<int>> &adj, vector<bool> &vis) {
+        vis[node] = true;
+        for(auto it : adj[node]) {
+            if(!vis[it]) {
+                if(dfs(it, node, adj, vis) == true) return true;
+            }
+            
+            else if(it != par) return true;
+        }
+        return false;
+    }
+    
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+        int n = edges.size();
+        vector<vector<int>> adj(n+1);
+        vector<bool> vis(n+1, false);
+        
+        for(auto it : edges) {
+            fill(begin(vis), end(vis), 0);
+            adj[it[0]].push_back(it[1]);
+            adj[it[1]].push_back(it[0]);
+            if(dfs(it[0], -1, adj, vis) == true) return it;
+        }
+        return {};
     }
 };
