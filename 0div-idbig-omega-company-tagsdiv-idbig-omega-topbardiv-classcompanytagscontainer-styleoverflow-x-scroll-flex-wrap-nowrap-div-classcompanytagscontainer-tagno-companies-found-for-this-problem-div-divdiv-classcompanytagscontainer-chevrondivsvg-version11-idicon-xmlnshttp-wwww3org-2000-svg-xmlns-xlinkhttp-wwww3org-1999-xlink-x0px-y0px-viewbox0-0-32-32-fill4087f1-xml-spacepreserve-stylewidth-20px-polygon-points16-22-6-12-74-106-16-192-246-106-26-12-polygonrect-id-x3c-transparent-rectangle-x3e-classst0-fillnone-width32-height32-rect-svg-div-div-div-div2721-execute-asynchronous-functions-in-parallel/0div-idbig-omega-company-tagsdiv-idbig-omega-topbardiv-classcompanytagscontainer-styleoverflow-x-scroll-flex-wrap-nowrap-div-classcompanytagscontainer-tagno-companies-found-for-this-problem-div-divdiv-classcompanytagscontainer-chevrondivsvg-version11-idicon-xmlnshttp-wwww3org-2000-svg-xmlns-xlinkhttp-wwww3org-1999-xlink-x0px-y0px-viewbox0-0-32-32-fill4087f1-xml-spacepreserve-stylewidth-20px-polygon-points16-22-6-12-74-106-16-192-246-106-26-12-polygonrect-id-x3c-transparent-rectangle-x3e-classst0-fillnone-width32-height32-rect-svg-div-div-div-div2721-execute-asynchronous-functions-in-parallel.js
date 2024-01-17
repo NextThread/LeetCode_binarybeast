0a8af81713efd14e482be42afe1150b9ffbafbfc
Anurag Roy <89125578+NextThread@users.noreply.github.com>
2024-01-17 23:06:50 +0530

    Time: 69 ms (62.38%), Space: 42.2 MB (58.79%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2721-execute-asynchronous-functions-in-parallel/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2721-execute-asynchronous-functions-in-parallel.js
@@ -0,0 +1,37 @@
+/**
+ * @param {Array<Function>} functions
+ * @return {Promise<any>}
+ */
+var promiseAll = function(functions) {
+  return new Promise((resolve, reject) => {
+    const results = [];
+    let resolvedCount = 0;
+    let rejected = false;
+
+    for (let i = 0; i < functions.length; i++) {
+      functions[i]()
+        .then(result => {
+          if (!rejected) {
+            results[i] = result;
+            resolvedCount++;
+
+            if (resolvedCount === functions.length) {
+              resolve(results);
+            }
+          }
+        })
+        .catch(error => {
+          if (!rejected) {
+            rejected = true;
+            reject(error);
+          }
+        });
+    }
+  });
+};
+
+
+/**
+ * const promise = promiseAll([() => new Promise(res => res(42))])
+ * promise.then(console.log); // [42]
+ */
