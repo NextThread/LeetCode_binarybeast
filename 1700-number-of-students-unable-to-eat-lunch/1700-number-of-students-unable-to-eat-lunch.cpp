a025d54d04b0ed148ae0617b81c7e3aed46689fc
Anurag Roy <89125578+NextThread@users.noreply.github.com>
2024-04-08 10:18:35 +0530

    Time: 5 ms (31.29%), Space: 10.7 MB (95.64%) - LeetHub

---
+++ 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int countStudents(vector<int>& A, vector<int>& B) {
+         int count[] = {0, 0}, n = A.size(), k;
+        for (int a : A)
+            count[a]++;
+        for (k = 0; k < n && count[B[k]] > 0; ++k)
+            count[B[k]]--;
+        return n - k;
+    }
+};
