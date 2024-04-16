afd94d8a51c39b1ee3fa5083c32b53de2e2a7834
Anurag Roy <89125578+NextThread@users.noreply.github.com>
2024-04-16 10:35:58 +0530

    Time: 8 ms (47.39%), Space: 15 MB (37.62%) - LeetHub

---
+++ 0768-max-chunks-to-make-sorted-ii/0768-max-chunks-to-make-sorted-ii.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxChunksToSorted(vector<int>& arr) {
+        long int sum1 = 0, sum2 = 0, ans = 0;
+        vector<int> t = arr;
+        sort(t.begin(), t.end());
+        for(int i = 0; i < arr.size(); i++) {
+            sum1 += t[i];
+            sum2 += arr[i];
+            if(sum1 == sum2) ans++;
+        }
+	return ans;
+    }
+};
