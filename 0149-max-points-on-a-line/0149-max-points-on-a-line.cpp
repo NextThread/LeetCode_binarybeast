33555815ef9788071171afb0fd4b177dcb05a21d
Anurag Roy <89125578+NextThread@users.noreply.github.com>
2024-05-26 11:01:23 +0530

    Time: 37 ms (45.99%), Space: 23.6 MB (24.73%) - LeetHub

--- 0149-max-points-on-a-line/0149-max-points-on-a-line.cpp
+++ 0149-max-points-on-a-line/0149-max-points-on-a-line.cpp
@@ -1,32 +1,50 @@
-class Solution{
-public :
+class Solution {
+public:
     
-    int maxPoints(vector<vector<int>>& points) 
-    {
-        if(points.size()<=2) return points.size();
-        int res=0;
-        int n=points.size();
-        for(int i=0;i<n;i++){
-
-            for(int j=i+1;j<n;j++){
-
+    // According to constraint we are allowed with maximum time complexity of O(n^3)
+    // 1 <= points.length <= 300
+    ///////   All the points are unique.
+    int maxPoints(vector<vector<int>>& points) {
+        
+        if(points.size()<=2) return points.size(); // only two points are always on the same line, beacuse using 2 points
+                                                    // line is made
+        int res = 0;
+        for(int i = 0; i < points.size(); i++)
+        {
+            unordered_map<double, int> mp;
+            int duplicate = 0;
+            double slope = 0.0;
+            for(int j = 0; j < points.size(); j++)
+            {
                 int x1 = points[i][0];
                 int x2 = points[j][0];
                 int y1 = points[i][1];
                 int y2 = points[j][1];    
-
-                // double slope = double(y2 - y1)/double(x2 - x1);
-                int total=2;
-                for(int k=0;k<n && k!=i && k!=j;k++){
-                    
-                    int x = points[k][0];
-                    int y = points[k][1];
-                    // third point
-                    // double(y - y1)/double(x1 - x1)
-                    if((y2 - y1)*(x - x1) == (x2 - x1)*(y - y1))
-                        total++;
+                
+                // slope = dy/dx.
+                int dy = y2 - y1;
+                int dx = x2 - x1;
+                if(dy == 0 && dx == 0){  // same overlapping point --> consider 2 same point as single point
+                    duplicate++; 
+                    continue;
                 }
-                res = max(res, total);
+                
+                if(dx != 0)
+                    slope = dy*1.0/dx; // store in double
+                else // dx==0 means slope is infinity
+                    slope = INT_MAX;
+                
+                mp[slope]++;
+            }
+
+            if(mp.size() == 0)
+                res = duplicate;
+            else
+            {
+                for(auto slope : mp){
+                    
+                    res = max(res, slope.second + duplicate);
+                }
             }
         }
         return res;
